da5eeb21e2850755c4922309a2ed8d37d7c1f705
simo ghrissi <mohammed.ghrissi@engie.com>
2017-12-30 18:45:26 +0100

    profil

--- EyesApp/Profil/UI/ProfilTableViewController.swift
+++ EyesApp/Profil/UI/ProfilTableViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
 
 class ProfilTableViewController: UITableViewController {
-
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var phoneText: DesignableTextField!
+    @IBOutlet weak var addressText: DesignableTextField!
+    @IBOutlet weak var passwordText: DesignableTextField!
+    @IBOutlet weak var birthDayText: DesignableTextField!
+    @IBOutlet weak var genderText: DesignableTextField!
+    @IBOutlet weak var emailText: DesignableTextField!
+    @IBOutlet weak var lastNameText: UITextField!
+    @IBOutlet weak var firstNameText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupBinding()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -20,4 +33,15 @@ class ProfilTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    @IBAction func saveAction(_ sender: Any) {
+    
+    }
+    
+    @IBAction func changeProfilAction(_ sender: Any) {
+    }
+    
+    func setupBinding(){
+        
+    }
+    
 }
---
+++ EyesApp/Profil/ViewModel/ProfilViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilViewModel.swift
+//  EyesApp
+//
+//  Created by simo on 30/12/2017.
+//  Copyright © 2017 simo. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class ProfilViewModel {
+
+    
+    init() {
+        
+    }
+}
